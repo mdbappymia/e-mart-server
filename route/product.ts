f887adb459246310bccb246069e8281ff80ee82e
mdbappymia <mbm.21.02.16@gmail.com>
2022-10-02 02:27:17 +0600

    add product

--- route/product.ts
+++ route/product.ts
@@ -6,40 +6,49 @@ import rimraf from "rimraf";
 export const product = Router();
 
 product.get("/", async (req, res) => {
-  await client.connect();
-  console.log("Database connected");
-  const products = await client
-    .db("product_management")
-    .collection("products")
-    .find()
-    .toArray();
-  res.json(products);
+  try {
+    await client.connect();
+    console.log("Database connected");
+    const products = await client
+      .db("product_management")
+      .collection("products")
+      .find()
+      .toArray();
+    res.json(products);
+  } catch (e) {
+    res.send(new Error("error"));
+  }
 });
 product.post("/add", async (req, res) => {
-  await client.connect();
-  console.log("database connected");
-  if (req.method === "POST") {
-    const database = client.db("product_management");
-    const productCollection = database.collection("products");
-    const data = req.body;
-    const result = await productCollection.insertOne(data);
-    res.json(result);
+  try {
+    await client.connect();
+    console.log("database connected");
+    if (req.method === "POST") {
+      const database = client.db("product_management");
+      const productCollection = database.collection("products");
+      const data = req.body;
+      const result = await productCollection.insertOne(data);
+      res.json(result);
+    }
+  } catch (err) {
+    res.send(new Error("Error"));
   }
 });
 
 product.post("/image", async (req: any, res) => {
   try {
+    console.log(req.body);
     const path = `public/utils/images/${req.body.folder}`;
     fs.mkdir(path, { recursive: true }, (error: any) => {
       if (error) {
         res.send(error.message);
       }
       req.files.image.mv(
-        `public/utils/images/${req.body.folder}/${req.files.image.name}`
+        `public/utils/images/${req.body.folder}/${req.body.id}.jpg`
       );
     });
     res.json({
-      url: `public/utils/images/${req.body.folder}/${req.files.image.name}`,
+      url: `public/utils/images/${req.body.folder}/${req.body.id}.jpg`,
     });
   } catch (e: any) {
     res.send(e.message);
@@ -48,9 +57,22 @@ product.post("/image", async (req: any, res) => {
 
 product.delete("/image", async (req, res) => {
   console.log(req.body);
-  rimraf(`${req.body.url}`, async function (e) {
-    if (e) {
-      throw new Error("Error");
-    } else res.status(200).json({ acknowledged: true, url: req.body.url });
-  });
+  try {
+    rimraf(`${req.body.url}`, async function (e) {
+      if (e) {
+        throw new Error("Error");
+      } else res.status(200).json({ acknowledged: true, url: req.body.url });
+    });
+  } catch (e) {
+    res.send(new Error("Error"));
+  }
+});
+
+product.get("/category", async (req, res) => {
+  await client.connect();
+  const category = await client
+    .db("product_management")
+    .collection("products")
+    .distinct("category");
+  res.send(category);
 });
